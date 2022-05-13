e9fcfa66ece869d3c531915168870d1861e1b835
Nomena Razakamalala <noumrazaka@gmail,com>
2022-05-13 08:54:56 +0300

    feat:register and login

---
+++ controllers/userController.js
@@ -0,0 +1,58 @@
+const { User } = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.getUsers = async(req, res) => {
+    const userList = await User.find().select('-passwordHash');
+
+    if (!userList) {
+        res.status(500).json({ success: false })
+    }
+    res.send(userList);
+}
+
+exports.getUsersById = async(req, res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if (!user) {
+        res.status(500).json({ message: 'The user with the given ID was not found.' })
+    }
+    res.status(200).send(user);
+}
+
+exports.register = async(req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+    })
+    user = await user.save();
+
+    if (!user)
+        return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+}
+
+exports.login = async(req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+    const secret = process.env.secret;
+    if (!user) {
+        return res.status(400).send('The user not found');
+    }
+
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign({
+                userId: user.id,
+                isAdmin: user.isAdmin
+            },
+            secret, { expiresIn: '1d' }
+        )
+
+        res.status(200).send({ user: user.email, token: token }) //
+    } else {
+        res.status(400).send('password is wrong!');
+    }
+}
